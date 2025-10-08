98bc103a217c1c51dd54366cea9fcc6f1da0a92d
SEHUZ <sonic15622@gmail.com>
2025-10-08 03:19:33 -0700

    Definir entidades

---
+++ CasodeUso3_252115/src/main/java/com/mycompany/casodeuso3_252115/CasodeUso3_252115.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.casodeuso3_252115;
+
+/**
+ *
+ * @author sonic
+ */
+public class CasodeUso3_252115 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
