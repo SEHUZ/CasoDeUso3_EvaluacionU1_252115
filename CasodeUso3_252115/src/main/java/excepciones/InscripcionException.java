a96e0576f71f51efabb656be6c9c6cd33110db4e
SEHUZ <sonic15622@gmail.com>
2025-10-10 11:45:02 -0700

    Excepcion personalizada para manejar excepciones variadas

---
+++ CasodeUso3_252115/src/main/java/excepciones/InscripcionException.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+/**
+ *
+ * @author sonic
+ */
+public class InscripcionException extends Exception {
+
+    public InscripcionException() {
+    }
+
+    public InscripcionException(String message) {
+        super(message);
+    }
+
+    public InscripcionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
+
